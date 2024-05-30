1015435630f3b3361c46cfa2349b17f18b23e669
UKNAPEN <uknapen@gmail.com>
2024-05-30 10:53:38 +0200

    API + Layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
 }
